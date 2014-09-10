c752339792215163c544320468f496a0927433a8
unknown <INShettS@INRD-SRIDARSHAN.apac.corp.natinst.com>
2014-09-10 14:54:41 +0530

    Minor edits

--- grabber.py
+++ grabber.py
@@ -3,7 +3,7 @@ import sys
 import os, socket
 from bs4 import BeautifulSoup
 
-socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(30)
 
 def getTodayIndex():
     url = "http://explosm.net/comics/new"
@@ -14,7 +14,7 @@ def getTodayIndex():
     return int(today)
 
 def startGrabbing(count):
-    day = getTodayIndex() 
+    day = getTodayIndex()
     for i in xrange(count):
         url = "http://explosm.net/comics/" + `day`
         try:
@@ -27,23 +27,32 @@ def startGrabbing(count):
                     image_link = image['src']
                     if "play-button" not in image_link:
                         try:
-		            print "image link : " + image_link
-		            image_response = urllib2.urlopen(image_link)
-		            image_data = image_response.read()
-		            f = open(os.path.join("comics",`i`), "w")
-		            f.write(image_data)
-		            f.close()
-		        except:
-		            pass
+                            print "image link : " + image_link
+                            image_response = urllib2.urlopen(image_link)
+                            image_data = image_response.read()
+                            splits = image_link.split('/')
+                            author_filename = splits[len(splits)-2:]
+                            write_file_name = `i`
+                            if len(author_filename) == 2:
+                                author, filename = author_filename
+                                write_file_name = author + "_" + filename
+                            f = open(os.path.join("comics",write_file_name), 'wb')
+                            f.write(image_data)
+                            f.close()
+                        except:
+                            pass
                         print "+"
         except:
             pass
         day -= 1
-    
-	
+
+
 
 if __name__ == "__main__":
     count = 10
     if len(sys.argv) > 1:
         count = int(sys.argv[1])
+    path = r'comics'
+    if not os.path.exists(path):
+        os.makedirs(path)
     startGrabbing(count)
